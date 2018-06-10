00565d59744686fa006590db1f46cc93c99a9d19
moonlight <442110511@qq.com>
2018-06-10 22:39:36 +0800

    chapter-04

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,5 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from django.http import HttpResponse
 from django.shortcuts import render
+# Create your views here.
 
 def home_page(request):
-    return render(request,'home.html',{'new_item_text':request.POST.get('item_text',"")})
-
-# Create your views here.
+    return render(request, 'home.html')
