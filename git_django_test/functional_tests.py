614d6ad45c6b7db4e01794dc787ce0fef23a150e
moonlight <442110511@qq.com>
2018-06-11 23:19:40 +0800

    'test02'

--- git_django_test/functional_tests.py
+++ git_django_test/functional_tests.py
@@ -1,67 +1,36 @@
-from django.test import LiveServerTestCase
 from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
+import unittest
 import time
-
-MAX_WAIT = 5
-
-class NewVisitorTest(LiveServerTestCase):
+class NewVistorTest(unittest.TestCase):
     def setUp(self):
-        self.browser = webdriver.Chrome()
+        self.browser=webdriver.Chrome()
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edith has heard about a cool new online to-do app. She goes
-        # to check out its homepage
-        self.browser.get(self.live_server_url)
-        # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
-    # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
-        # She types "Buy peacock feathers" into a text box (Edith's hobby
-        # is tying fly-fishing lures)
+        self.browser.get("http://127.0.0.1:8000/")
+        self.assertIn('To-Do',self.browser.title)
+        header_text=self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
+
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
+
         inputbox.send_keys('Buy peacock feathers')
-        # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-        # There is still a text box inviting her to add another item. She
-
-        # enters "Use peacock feathers to make a fly" (Edith is very
-
-        # methodical)
+        table=self.browser.find_element_by_id('id_list_table')
+        rows=table.find_elements_by_tag_name('tr')
+        #self.assertTrue(any(row.text=='1:Buy peacock feathers' for row in rows),"New to-do item did not appear in table")
+        self .assertIn('1:Buy peacock feathers',[row.text for row in rows])
+        #self.fail('Finish the test')
 
         inputbox = self.browser.find_element_by_id('id_new_item')
-
         inputbox.send_keys('Use peacock feathers to make a fly')
-
         inputbox.send_keys(Keys.ENTER)
-
-        # The page updates again, and now shows both items on her list
-
-        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
-
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
-
-        # Edith wonders whether the site will remember her list. Then she sees
-
-        # that the site has generated a unique URL for her -- there is some
-
-        # explanatory text to that effect.
-
-        self.fail('Finish the test!')
-
-        # She visits that URL - her to-do list is still there.
-
-        # Satisfied, she goes back to sleep
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('2:Use peacock feathers to make a fly', [row.text for row in rows])
--- lists/tests.py
+++ lists/tests.py
@@ -16,5 +16,5 @@ class HomePageTest(TestCase):
         request.POST['item_text']='A new list item'
         response=home_page(request)
         #self.assertIn('A new list item',response.content.decode())
-        expected_html=render_to_string('home.html',{'new_item_text':'A new list item'},request=request)
+        expected_html=render_to_string('home.html',{'new_item_text':'A new list item'})
         self.assertEqual(response.content.decode(),expected_html)
