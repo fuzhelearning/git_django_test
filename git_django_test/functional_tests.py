1b45d3ec271f45e7213ebfe14b59044058ed43f5
moonlight <442110511@qq.com>
2018-06-13 00:03:18 +0800

    'data_base_test'

--- git_django_test/functional_tests.py
+++ git_django_test/functional_tests.py
@@ -10,6 +10,12 @@ class NewVistorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self,row_text):
+        table=self.browser.find_element_by_id('id_list_table')
+        rows=table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
+
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get("http://127.0.0.1:8000/")
         self.assertIn('To-Do',self.browser.title)
@@ -21,16 +27,9 @@ class NewVistorTest(unittest.TestCase):
 
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-
-        table=self.browser.find_element_by_id('id_list_table')
-        rows=table.find_elements_by_tag_name('tr')
-        #self.assertTrue(any(row.text=='1:Buy peacock feathers' for row in rows),"New to-do item did not appear in table")
-        self .assertIn('1:Buy peacock feathers',[row.text for row in rows])
-        #self.fail('Finish the test')
-
+        self.check_for_row_in_list_table('1:Buy peacock feathers')
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('2:Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('2:Use peacock feathers to make a fly')
+
--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.http import HttpRequest
 from lists.views import *
 from django.template.loader import render_to_string
 from django.core.urlresolvers import resolve
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -18,3 +19,22 @@ class HomePageTest(TestCase):
         #self.assertIn('A new list item',response.content.decode())
         expected_html=render_to_string('home.html',{'new_item_text':'A new list item'})
         self.assertEqual(response.content.decode(),expected_html)
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item=Item()
+        first_item.text='The first (ever) list item'
+        first_item.save()
+
+        second_item=Item()
+        second_item.text='Item the second'
+        second_item.save()
+
+        saved_items=Item.objects.all()
+        self.assertEqual(saved_items.count(),2)
+
+        first_saved_item=saved_items[0]
+        second_saved_item=saved_items[1]
+        self.assertEqual(first_saved_item.text,'The first (ever) list item')
+        self.assertEqual(second_saved_item,'Item the second')
