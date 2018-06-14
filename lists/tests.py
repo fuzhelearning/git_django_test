e22f82cbd0c08bc4937a7e14149a160e1e26abf7
moonlight <442110511@qq.com>
2018-06-15 00:02:05 +0800

    'modified'

--- lists/tests.py
+++ lists/tests.py
@@ -11,6 +11,19 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
+    def test_home_page_can_save_a_POST_request(self):
+        request=HttpRequest()
+        request.method='POST'
+        request.POST['item_text']='A new list item'
+        response=home_page(request)
+        self.assertEqual(Item.objects.count(),1)
+        new_item=Item.objects.first()
+        self.assertEqual(new_item.text,'A new list item')
+
+        self.assertIn('A new list item',response.content.decode())
+        expected_html=render_to_string('home.html',{'new_item_text':'A new list item'})
+        self.assertEqual(response.content.decode(),expected_html)
+
     def test_home_page_returns_correct_html(self):
         request=HttpRequest()
         request.method='POST'
