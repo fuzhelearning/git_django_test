e74dcb354d14ad3280f68ac35169348495512800
shopee <shopee@liuxiong.local>
2018-06-15 15:02:41 +0800

    test_home_page_only_saves_items_when_necessary_add

--- lists/tests.py
+++ lists/tests.py
@@ -33,6 +33,10 @@ class HomePageTest(TestCase):
         expected_html=render_to_string('home.html',{'new_item_text':'A new list item'})
         self.assertEqual(response.content.decode(),expected_html)
 
+    def test_home_page_only_saves_items_when_necessary(self):
+        request=HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(),0)
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
@@ -51,3 +55,5 @@ class ItemModelTest(TestCase):
         second_saved_item=saved_items[1]
         self.assertEqual(first_saved_item.text,'The first (ever) list item')
         self.assertEqual(second_saved_item,'Item the second')
+
+
