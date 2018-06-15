35264cea9e7c7badeb9de2e1cdc35851a4d542a8
shopee <shopee@liuxiong.local>
2018-06-15 14:50:52 +0800

    insert database

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
+from models import Item
+
 def home_page(request):
-    return render(request,'home.html',{'new_item_text':request.POST.get('item_text','')})
+    item=Item()
+    item.text=request.POST.get('item_text','')
+    item.save()
+
+    return render(request,'home.html',{'new_item_text':item.text})
