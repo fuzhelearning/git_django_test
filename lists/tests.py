0af1b5f7d282961a22204142cdb557a1d75180bd
shopee <shopee@liuxiong.local>
2018-06-15 16:04:20 +0800

    divided test lists

--- lists/tests.py
+++ lists/tests.py
@@ -20,9 +20,15 @@ class HomePageTest(TestCase):
         new_item=Item.objects.first()
         self.assertEqual(new_item.text,'A new list item')
 
-        self.assertIn('A new list item',response.content.decode())
-        expected_html=render_to_string('home.html',{'new_item_text':'A new list item'})
-        self.assertEqual(response.content.decode(),expected_html)
+
+    def test_home_page_redirects_after_POST(self):
+        request=HttpRequest()
+        request.method='POST'
+        request.POST['item_text']='A new list item'
+        response=home_page(request)
+        self.assertEqual(response.status_code,302)
+        self.assertEqual(response['location'],'/')
+
 
     def test_home_page_returns_correct_html(self):
         request=HttpRequest()
@@ -31,6 +37,9 @@ class HomePageTest(TestCase):
         response=home_page(request)
         #self.assertIn('A new list item',response.content.decode())
         expected_html=render_to_string('home.html',{'new_item_text':'A new list item'})
+        #print(response)
+        #print('_________________')
+        #print(expected_html)
         self.assertEqual(response.content.decode(),expected_html)
 
     def test_home_page_only_saves_items_when_necessary(self):
--- lists/views.py
+++ lists/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 # Create your views here.
 from django.http import HttpResponse
 from models import Item
 
 def home_page(request):
-    item=Item()
-    item.text=request.POST.get('item_text','')
-    item.save()
-
-    return render(request,'home.html',{'new_item_text':item.text})
+    if request.method=='POST':
+        new_item_text=request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+        return redirect('/')
+    return render(request,'home.html')
